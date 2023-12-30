24b6328d125055535c9e6313b984c75f042fc0d1
amratansh12 <ashri1205@gmail.com>
2023-12-30 13:41:29 +0530

    Adding blocking, follow and unfollow and unblocking functions

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,6 +1,10 @@
 "use client";
+import { useIsClient } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps{
   children: React.ReactNode;
@@ -10,6 +14,17 @@ export const Wrapper = ({
   children,
 }: WrapperProps) => {
   const {collapsed} = useSidebar((state)=>state);
+  const isClient = useIsClient();
+
+  if(!isClient) {
+    return(
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <FollowingSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
+  }
 
   return(
     <aside 
--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -57,7 +57,7 @@ export const UserAvatar = ({
         </AvatarFallback>
       </Avatar>
       {canShowBadge && (
-        <div className="absolute -bottom-3 left-1/2 transform-translate-x-1/2">
+        <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2">
           <LiveBadge /> 
         </div>
       )}
