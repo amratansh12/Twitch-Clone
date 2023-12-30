9862072eb6fa18b6bc70704fb3df2ff7f16ce7ce
amratansh12 <ashri1205@gmail.com>
2023-12-30 22:05:12 +0530

    Adding the dashboard functionalities

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -1,5 +1,6 @@
 import { currentUser } from "@clerk/nextjs"
 import { db } from "./db"
+import { error } from "console";
 
 export const getSelf = async () => {
   const self = await currentUser();
@@ -14,6 +15,30 @@ export const getSelf = async () => {
 
   if(!user){
     throw new Error("Not Found")
+  }
+
+  return user;
+}
+
+export const getSelfByUsername = async (username: string) => {
+  const self = await currentUser();
+
+  if(!self || !self.username) {
+    throw new Error("Unauthorized")
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username
+    }
+  });
+
+  if(!user) {
+    throw new Error("User not found");
+  }
+
+  if(self.username !== user.username) {
+    throw new Error("Unauthorized")
   }
 
   return user;
